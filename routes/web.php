27784e3f9a1a588c25fb8213deadb09d7cbec040
Amir-ben-mustapha <ben.mustapha.amir.5@gmail.com>
2024-04-15 20:11:32 +0100

    add extensions + navbar

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home.home');
+})->name('app_home');
+
+Route::get('/about', function () {
+    return view('home.about');
+})->name('app_about');
